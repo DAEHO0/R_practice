35c9010d831fdabba59398bdac4bd7943b144402
DAEHO0 <tigrix0316@naver.com>
2023-08-07 13:47:43 +0900

    중간저장2

--- 230807_ChiCor.R
+++ 230807_ChiCor.R
@@ -20,3 +20,33 @@ mean(df_gen1$할인민감여부)
 
 
 # 5. 상관관계 분석
+
+help(cor)
+cor(df$방문빈도, df$총매출액, method = "pearson")
+# 0.4737282 # 양의 상관관계이나 강하지 않다
+plot(df$방문빈도, df$총매출액)
+abline(lm(df$방문빈도 ~ df$총매출액), col = "blue")
+reg <- lm(총매출액 ~ 방문빈도, data = df)
+summary(reg)
+
+"""
+Call:
+lm(formula = 총매출액 ~ 방문빈도, data = df)
+
+Residuals:
+      Min        1Q    Median        3Q       Max 
+-18931365  -2152630   -895261    817111  52777858 
+
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  2551792     253151   10.08   <2e-16 ***
+방문빈도      144307       8492   16.99   <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 5122000 on 998 degrees of freedom
+Multiple R-squared:  0.2244,	Adjusted R-squared:  0.2236 
+F-statistic: 288.8 on 1 and 998 DF,  p-value: < 2.2e-16
+
+
+"""
