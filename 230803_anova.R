8013bae44fa3a996a8fb9371d44f16df3c17f069
DAEHO0 <tigrix0316@naver.com>
2023-08-08 15:22:00 +0900

    중간저장2

--- 230803_anova.R
+++ 230803_anova.R
@@ -9,7 +9,29 @@ versicolor <- data[data$Species == "versicolor", ]  # versicolor 변수에 Species�
 str(setosa) # setosa 변수의 구조
 str(versicolor) # versicolor 변수의 구조
 
-anova <- aov(Sepal.Length ~ Species, data = data)
+anova <- aov(Sepal.Length ~ Species, # Sepal.Length(꽃받침 길이)를 Species(종)에 따라 분석하고자 함
+                                     # 종속변수 ~ 독립변수 형식으로 사용, 종에 따라 꽃받침 길이가 어떻게 다른지 분석
+                                     # anova(분산분석) 결과를 anova 변수에 저장
+             data = data) # 데이터는 iris 데이터가 저장된 data 변수를 사용
+
+# 붓꽃의 종류에 따라서 꽃받침 길이는 다를까?
+
+anova
+
+"""
+Call:
+   aov(formula = Sepal.Length ~ Species, data = data)
+
+Terms:
+                 Species Residuals
+Sum of Squares  63.21213  38.95620  # Species, Residuals 각각의 제곱합
+Deg. of Freedom        2       147  # Species의 자유도는 2, 오차에 대한 자유도 147
+
+Residual standard error: 0.5147894  # 잔차표준오차(RSE): 회귀분석에서 예측된 종속 변수 값과 
+                                    # 실제 관측된 종속 변수 값 사이의 차이를 나타내는 잔차들의 표준 편차
+Estimated effects may be unbalanced
+
+"""
 summary(anova)
 
 """
