46851ee786098a06957fd0d7084b5011558f955d
DAEHO0 <tigrix0316@naver.com>
2023-08-10 11:48:11 +0900

    3rd commit

--- 230809_chatGPT_re.R
+++ 230809_chatGPT_re.R
@@ -31,3 +31,41 @@ lower <- q1 - 1.5 * iqr
 upper <- q3 + 1.5 * iqr
 data_no_out <- b[b$age >= lower & b$age <= upper, ]
 data_no_out
+
+
+### 중복값 처리 ###
+
+# 데이터 로딩
+d <- data.frame(id = c(1, 2, 3, 1, 4),
+                score = c(90, 85, 78, 90, 92))
+
+# 중복값 확인
+dupl_rows <- d[duplicated(d), ]
+dupl_rows
+# id score
+# 4  1    90 <- 중복값
+
+# 중복값 제거
+uniq_data <- unique(d)
+uniq_data
+
+
+### 정규화 ###
+
+# 데이터 로딩
+echo <- data.frame(age = c(25, 30, 22, 25, 40),
+                   income = c(50000, 60000, 75000, 80000, 90000))
+
+# 최소-최대 정규화
+mn_mx_norm <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+norm_data <- as.data.frame(lapply(data, mn_mx_norm))
+# Error in Summary.factor(c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, :
+# ‘min’ not meaningful for factors
+
+# Z-점수 정규화
+z_score_norm <- function(x) {
+  return((x - mean(x)) / sd(x))
+}
