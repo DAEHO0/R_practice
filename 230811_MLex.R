e465a73548eef693708339276234bbdcc0a602e6
DAEHO0 <tigrix0316@naver.com>
2023-08-11 17:50:29 +0900

    왜 갑자기 한글 깨짐?

--- 230811_MLex.R
+++ 230811_MLex.R
@@ -1,1 +1,18 @@
-### ¸Ó½Å·¯´× ###
+### Machine Learning ###
+
+alpha <- read.csv("Ashopping.csv", fileEncoding = "EUC-KR")
+View(alpha)
+
+library(ggplot2)
+library(lattice)
+library(caret)
+# Loading required package ggplot2 and lattice
+
+set.seed(123)
+# inTrain <- createDataPartition(data.frame$factor, p = .75, list = FALSE)
+
+str(alpha$°í°´µî±Þ) # integer
+
+alpha$°í°´µî±Þ <- as.factor(alpha$°í°´µî±Þ) # factor·Î º¯È¯
+str(alpha$°í°´µî±Þ) # factor
+
