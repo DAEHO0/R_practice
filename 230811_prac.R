bb6d739824971fa4e438bfc0d09cd4bcac56f62b
DAEHO0 <tigrix0316@naver.com>
2023-08-11 10:23:49 +0900

    2nd commit, 과제 있음

--- 230811_prac.R
+++ 230811_prac.R
@@ -7,3 +7,26 @@ getwd()
 # data 불러오기
 df <- read.csv("Ashopping.csv", fileEncoding = "EUC-KR")
 str(df)
+View(df)
+
+# 평균검정(t-검정, ANOVA)
+clm0 <- df[df$클레임접수여부 == 0, ]
+clm1 <- df[df$클레임접수여부 == 1, ]
+str(clm0)
+str(clm1)
+
+# 정규성
+shapiro.test(clm0$총매출액)
+shapiro.test(clm1$총매출액)
+
+# Wilcoxon
+wilcox.test(clm1$총매출액, clm1$총매출액)
+median(clm0$총매출액)
+median(clm1$총매출액)
+
+# anova(3 집단 이상)
+colnames(df)
+anova <- aov(총매출액 ~ 구매금액대, data = df)
+summary(anova)
+
+# 크루스칼-왈리스 검정(과제)
