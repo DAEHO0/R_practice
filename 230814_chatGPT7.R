bded037b0d42c41f6a1bec0d70e03a8d2b03466f
DAEHO0 <tigrix0316@naver.com>
2023-08-14 17:17:31 +0900

    genr commit

---
+++ 230814_chatGPT7.R
@@ -0,0 +1,20 @@
+### 앙상블 ###
+
+# 라이브러리 불러오기
+install.packages('randomForest')
+library(randomForest)
+
+# 데이터 로드
+data(iris)
+train_data <- iris[1:100, -5]
+train_label <-iris[1:100, 5]
+
+# 랜덤 포레스트 모델 생성
+rf_mdl <- randomForest(train_data, train_label)
+# Error in randomForest.default(train_data, train_label) : 
+#   Can't have empty classes in y.
+
+# 예측
+predictions <- predict(rf_mdl, newdata = iris[101, 150], -5)
+
+# 예측 결과 출력
